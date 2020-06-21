72690d91e01b9879059c6443b93471bd01ccec2c
Catherine Straus <vnair611@gmail.com>
2020-06-21 01:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
