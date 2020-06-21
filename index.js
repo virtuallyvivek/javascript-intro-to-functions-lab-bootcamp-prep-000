8c9c6cfe759ae2f020fc788af5c142b5254e7b69
Catherine Straus <vnair611@gmail.com>
2020-06-21 01:24:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"; 
   }
   if (string === string.toUpperCase()) {
-    return "YES INDEED"; 
+    return "YES INDEED!"; 
   }
-  if (string === "I love you, Grandma"){
-    return "I love you too";
+  if (string === "I love you, Grandma."){
+    return "I love you too.";
   }
 }
